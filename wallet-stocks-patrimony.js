de6332dce9acbea0e50076de6acd5c0a4943cbb5
Josias Mota Soares <josias.mota.soares@gmail.com>
2022-09-13 17:12:54 -0300

    Update wallet-stocks-patrimony.js

--- wallet-stocks-patrimony.js
+++ wallet-stocks-patrimony.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name         Carteira -> Ações
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://statusinvest.com.br/carteira/patrimonio
-// @match        https://statusinvest.com.br/carteira/patrimonio?a=*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=statusinvest.com.br
-// @grant        none
-// ==/UserScript==
 setTimeout(function() {
     insertValuation()
 }, 1000);
